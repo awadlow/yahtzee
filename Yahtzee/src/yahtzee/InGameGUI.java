e0d9c8d485e640cfd47549c1f09a6b36a87c2a36
Casey <caok29@yahoo.com>
2016-04-07 00:48:34 -0700

    Designed first draft GUI. <TODO: Link validation functions to GUI>

--- Yahtzee/src/yahtzee/InGameGUI.java
+++ Yahtzee/src/yahtzee/InGameGUI.java
@@ -1,3 +1,7 @@
+/**
+ * @author CaseyO'Kelly
+ */
+
 package yahtzee;
 
 import java.awt.BorderLayout;
--- Yahtzee/src/yahtzee/PlayPanel.java
+++ Yahtzee/src/yahtzee/PlayPanel.java
@@ -1,9 +1,18 @@
+/**
+ * @author CaseyO'Kelly
+ */
+
 package yahtzee;
 
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
 
 public class PlayPanel extends JPanel {
 
@@ -12,17 +21,290 @@ public class PlayPanel extends JPanel {
 	 */
 	public PlayPanel() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		JLabel lblYahtzee = new JLabel("Yahtzee!");
 		GridBagConstraints gbc_lblYahtzee = new GridBagConstraints();
-		gbc_lblYahtzee.gridx = 7;
+		gbc_lblYahtzee.gridwidth = 8;
+		gbc_lblYahtzee.insets = new Insets(0, 0, 5, 5);
+		gbc_lblYahtzee.gridx = 2;
 		gbc_lblYahtzee.gridy = 1;
 		add(lblYahtzee, gbc_lblYahtzee);
+		
+		JLabel lblDieRollOne = new JLabel("1");
+		GridBagConstraints gbc_lblDieRollOne = new GridBagConstraints();
+		gbc_lblDieRollOne.gridwidth = 2;
+		gbc_lblDieRollOne.gridheight = 2;
+		gbc_lblDieRollOne.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDieRollOne.gridx = 1;
+		gbc_lblDieRollOne.gridy = 2;
+		add(lblDieRollOne, gbc_lblDieRollOne);
+		
+		JLabel lblDieRollTwo = new JLabel("2");
+		GridBagConstraints gbc_lblDieRollTwo = new GridBagConstraints();
+		gbc_lblDieRollTwo.gridwidth = 2;
+		gbc_lblDieRollTwo.gridheight = 2;
+		gbc_lblDieRollTwo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDieRollTwo.gridx = 3;
+		gbc_lblDieRollTwo.gridy = 2;
+		add(lblDieRollTwo, gbc_lblDieRollTwo);
+		
+		JLabel lblDieRollThree = new JLabel("3");
+		GridBagConstraints gbc_lblDieRollThree = new GridBagConstraints();
+		gbc_lblDieRollThree.gridwidth = 2;
+		gbc_lblDieRollThree.gridheight = 2;
+		gbc_lblDieRollThree.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDieRollThree.gridx = 5;
+		gbc_lblDieRollThree.gridy = 2;
+		add(lblDieRollThree, gbc_lblDieRollThree);
+		
+		JLabel lblDieRollFour = new JLabel("4");
+		GridBagConstraints gbc_lblDieRollFour = new GridBagConstraints();
+		gbc_lblDieRollFour.gridwidth = 2;
+		gbc_lblDieRollFour.gridheight = 2;
+		gbc_lblDieRollFour.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDieRollFour.gridx = 7;
+		gbc_lblDieRollFour.gridy = 2;
+		add(lblDieRollFour, gbc_lblDieRollFour);
+		
+		JLabel lblDieRollFive = new JLabel("5");
+		GridBagConstraints gbc_lblDieRollFive = new GridBagConstraints();
+		gbc_lblDieRollFive.gridwidth = 2;
+		gbc_lblDieRollFive.gridheight = 2;
+		gbc_lblDieRollFive.insets = new Insets(0, 0, 5, 0);
+		gbc_lblDieRollFive.gridx = 9;
+		gbc_lblDieRollFive.gridy = 2;
+		add(lblDieRollFive, gbc_lblDieRollFive);
+		
+		JCheckBox chckbxHoldDieOne = new JCheckBox("HoldDieOne");
+		GridBagConstraints gbc_chckbxHoldDieOne = new GridBagConstraints();
+		gbc_chckbxHoldDieOne.gridwidth = 2;
+		gbc_chckbxHoldDieOne.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxHoldDieOne.gridx = 1;
+		gbc_chckbxHoldDieOne.gridy = 4;
+		add(chckbxHoldDieOne, gbc_chckbxHoldDieOne);
+
+		JCheckBox chckbxHoldDieTwo = new JCheckBox("HoldDieOne");
+		GridBagConstraints gbc_chckbxHoldDieTwo = new GridBagConstraints();
+		gbc_chckbxHoldDieTwo.gridwidth = 2;
+		gbc_chckbxHoldDieTwo.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxHoldDieTwo.gridx = 3;
+		gbc_chckbxHoldDieTwo.gridy = 4;
+		add(chckbxHoldDieTwo, gbc_chckbxHoldDieTwo);
+		
+		JCheckBox chckbxHoldDieThree = new JCheckBox("HoldDieOne");
+		GridBagConstraints gbc_chckbxHoldDieThree = new GridBagConstraints();
+		gbc_chckbxHoldDieThree.gridwidth = 2;
+		gbc_chckbxHoldDieThree.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxHoldDieThree.gridx = 5;
+		gbc_chckbxHoldDieThree.gridy = 4;
+		add(chckbxHoldDieThree, gbc_chckbxHoldDieThree);
+		
+		JCheckBox chckbxHoldDieFour = new JCheckBox("HoldDieOne");
+		GridBagConstraints gbc_chckbxHoldDieFour = new GridBagConstraints();
+		gbc_chckbxHoldDieFour.gridwidth = 2;
+		gbc_chckbxHoldDieFour.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxHoldDieFour.gridx = 7;
+		gbc_chckbxHoldDieFour.gridy = 4;
+		add(chckbxHoldDieFour, gbc_chckbxHoldDieFour);
+		
+		JCheckBox chckbxHoldDieFive = new JCheckBox("HoldDieOne");
+		GridBagConstraints gbc_chckbxHoldDieFive = new GridBagConstraints();
+		gbc_chckbxHoldDieFive.gridwidth = 2;
+		gbc_chckbxHoldDieFive.insets = new Insets(0, 0, 5, 0);
+		gbc_chckbxHoldDieFive.gridx = 9;
+		gbc_chckbxHoldDieFive.gridy = 4;
+		add(chckbxHoldDieFive, gbc_chckbxHoldDieFive);
+		
+		JButton btnOnes = new JButton("ONES:");
+		GridBagConstraints gbc_btnOnes = new GridBagConstraints();
+		gbc_btnOnes.insets = new Insets(0, 0, 5, 5);
+		gbc_btnOnes.gridx = 2;
+		gbc_btnOnes.gridy = 5;
+		add(btnOnes, gbc_btnOnes);
+		
+		JLabel lblOnesScore = new JLabel("Ones Score");
+		GridBagConstraints gbc_lblOnesScore = new GridBagConstraints();
+		gbc_lblOnesScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblOnesScore.gridx = 3;
+		gbc_lblOnesScore.gridy = 5;
+		add(lblOnesScore, gbc_lblOnesScore);
+		
+		JButton btnFives = new JButton("FIVES:");
+		GridBagConstraints gbc_btnFives = new GridBagConstraints();
+		gbc_btnFives.insets = new Insets(0, 0, 5, 5);
+		gbc_btnFives.gridx = 5;
+		gbc_btnFives.gridy = 5;
+		add(btnFives, gbc_btnFives);
+		
+		JLabel lblFivesScore = new JLabel("Fives Score");
+		GridBagConstraints gbc_lblFivesScore = new GridBagConstraints();
+		gbc_lblFivesScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFivesScore.gridx = 6;
+		gbc_lblFivesScore.gridy = 5;
+		add(lblFivesScore, gbc_lblFivesScore);
+		
+		JLabel lblFullHouseScore = new JLabel("Full House Score");
+		GridBagConstraints gbc_lblFullHouseScore = new GridBagConstraints();
+		gbc_lblFullHouseScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFullHouseScore.gridx = 9;
+		gbc_lblFullHouseScore.gridy = 5;
+		add(lblFullHouseScore, gbc_lblFullHouseScore);
+		
+		JButton btnFullHouse = new JButton("FULL HOUSE:");
+		GridBagConstraints gbc_btnFullHouse = new GridBagConstraints();
+		gbc_btnFullHouse.insets = new Insets(0, 0, 5, 5);
+		gbc_btnFullHouse.gridx = 8;
+		gbc_btnFullHouse.gridy = 5;
+		add(btnFullHouse, gbc_btnFullHouse);
+		
+		JButton btnTwos = new JButton("TWOS:");
+		GridBagConstraints gbc_btnTwos = new GridBagConstraints();
+		gbc_btnTwos.insets = new Insets(0, 0, 5, 5);
+		gbc_btnTwos.gridx = 2;
+		gbc_btnTwos.gridy = 6;
+		add(btnTwos, gbc_btnTwos);
+		
+		JLabel lblTwosScore = new JLabel("Twos Score");
+		GridBagConstraints gbc_lblTwosScore = new GridBagConstraints();
+		gbc_lblTwosScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTwosScore.gridx = 3;
+		gbc_lblTwosScore.gridy = 6;
+		add(lblTwosScore, gbc_lblTwosScore);
+		
+		JButton btnSixes = new JButton("SIXES:");
+		GridBagConstraints gbc_btnSixes = new GridBagConstraints();
+		gbc_btnSixes.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSixes.gridx = 5;
+		gbc_btnSixes.gridy = 6;
+		add(btnSixes, gbc_btnSixes);
+		
+		JLabel lblSixesScore = new JLabel("Sixes Score");
+		GridBagConstraints gbc_lblSixesScore = new GridBagConstraints();
+		gbc_lblSixesScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSixesScore.gridx = 6;
+		gbc_lblSixesScore.gridy = 6;
+		add(lblSixesScore, gbc_lblSixesScore);
+		
+		JButton btnSmallStraight = new JButton("SMALL STRAIGHT:");
+		GridBagConstraints gbc_btnSmallStraight = new GridBagConstraints();
+		gbc_btnSmallStraight.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSmallStraight.gridx = 8;
+		gbc_btnSmallStraight.gridy = 6;
+		add(btnSmallStraight, gbc_btnSmallStraight);
+		
+		JLabel lblSmallStraightScore = new JLabel("Small Straight Score");
+		GridBagConstraints gbc_lblSmallStraightScore = new GridBagConstraints();
+		gbc_lblSmallStraightScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSmallStraightScore.gridx = 9;
+		gbc_lblSmallStraightScore.gridy = 6;
+		add(lblSmallStraightScore, gbc_lblSmallStraightScore);
+		
+		JButton btnThrees = new JButton("THREES:");
+		GridBagConstraints gbc_btnThrees = new GridBagConstraints();
+		gbc_btnThrees.insets = new Insets(0, 0, 5, 5);
+		gbc_btnThrees.gridx = 2;
+		gbc_btnThrees.gridy = 7;
+		add(btnThrees, gbc_btnThrees);
+		
+		JLabel lblThreesScore = new JLabel("Threes Score");
+		GridBagConstraints gbc_lblThreesScore = new GridBagConstraints();
+		gbc_lblThreesScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblThreesScore.gridx = 3;
+		gbc_lblThreesScore.gridy = 7;
+		add(lblThreesScore, gbc_lblThreesScore);
+		
+		JButton btn3OfAKind = new JButton("3 OF A KIND:");
+		GridBagConstraints gbc_btn3OfAKind = new GridBagConstraints();
+		gbc_btn3OfAKind.insets = new Insets(0, 0, 5, 5);
+		gbc_btn3OfAKind.gridx = 5;
+		gbc_btn3OfAKind.gridy = 7;
+		add(btn3OfAKind, gbc_btn3OfAKind);
+		
+		JLabel lbl3OfAKindScore = new JLabel("3 Of a Kind Score");
+		GridBagConstraints gbc_lbl3OfAKindScore = new GridBagConstraints();
+		gbc_lbl3OfAKindScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lbl3OfAKindScore.gridx = 6;
+		gbc_lbl3OfAKindScore.gridy = 7;
+		add(lbl3OfAKindScore, gbc_lbl3OfAKindScore);
+		
+		JButton btnLargeStraight = new JButton("LARGE STRAIGHT:");
+		GridBagConstraints gbc_btnLargeStraight = new GridBagConstraints();
+		gbc_btnLargeStraight.insets = new Insets(0, 0, 5, 5);
+		gbc_btnLargeStraight.gridx = 8;
+		gbc_btnLargeStraight.gridy = 7;
+		add(btnLargeStraight, gbc_btnLargeStraight);
+		
+		JLabel lblLargeStraightScore = new JLabel("Large Straight Score");
+		GridBagConstraints gbc_lblLargeStraightScore = new GridBagConstraints();
+		gbc_lblLargeStraightScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLargeStraightScore.gridx = 9;
+		gbc_lblLargeStraightScore.gridy = 7;
+		add(lblLargeStraightScore, gbc_lblLargeStraightScore);
+		
+		JButton btnFours = new JButton("FOURS:");
+		GridBagConstraints gbc_btnFours = new GridBagConstraints();
+		gbc_btnFours.insets = new Insets(0, 0, 5, 5);
+		gbc_btnFours.gridx = 2;
+		gbc_btnFours.gridy = 8;
+		add(btnFours, gbc_btnFours);
+		
+		JLabel lblFoursScore = new JLabel("Fours Score");
+		GridBagConstraints gbc_lblFoursScore = new GridBagConstraints();
+		gbc_lblFoursScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFoursScore.gridx = 3;
+		gbc_lblFoursScore.gridy = 8;
+		add(lblFoursScore, gbc_lblFoursScore);
+		
+		JButton btn4OfAKind = new JButton("4 OF A KIND:");
+		btn4OfAKind.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btn4OfAKind = new GridBagConstraints();
+		gbc_btn4OfAKind.insets = new Insets(0, 0, 5, 5);
+		gbc_btn4OfAKind.gridx = 5;
+		gbc_btn4OfAKind.gridy = 8;
+		add(btn4OfAKind, gbc_btn4OfAKind);
+		
+		JLabel lbl4OfAKindScore = new JLabel("4 Of a Kind Score");
+		GridBagConstraints gbc_lbl4OfAKindScore = new GridBagConstraints();
+		gbc_lbl4OfAKindScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lbl4OfAKindScore.gridx = 6;
+		gbc_lbl4OfAKindScore.gridy = 8;
+		add(lbl4OfAKindScore, gbc_lbl4OfAKindScore);
+		
+		JButton btnYahtzee = new JButton("YAHTZEE");
+		GridBagConstraints gbc_btnYahtzee = new GridBagConstraints();
+		gbc_btnYahtzee.insets = new Insets(0, 0, 5, 5);
+		gbc_btnYahtzee.gridx = 8;
+		gbc_btnYahtzee.gridy = 8;
+		add(btnYahtzee, gbc_btnYahtzee);
+		
+		JLabel lblYahtzeeScore = new JLabel("Yahtzee Score");
+		GridBagConstraints gbc_lblYahtzeeScore = new GridBagConstraints();
+		gbc_lblYahtzeeScore.insets = new Insets(0, 0, 5, 5);
+		gbc_lblYahtzeeScore.gridx = 9;
+		gbc_lblYahtzeeScore.gridy = 8;
+		add(lblYahtzeeScore, gbc_lblYahtzeeScore);
+		
+		JLabel lblOfRerolls = new JLabel("# OF REROLLS LEFT: 2");
+		GridBagConstraints gbc_lblOfRerolls = new GridBagConstraints();
+		gbc_lblOfRerolls.insets = new Insets(0, 0, 5, 5);
+		gbc_lblOfRerolls.gridx = 6;
+		gbc_lblOfRerolls.gridy = 9;
+		add(lblOfRerolls, gbc_lblOfRerolls);
+		
+		JButton btnRoll = new JButton("ROLL");
+		GridBagConstraints gbc_btnRoll = new GridBagConstraints();
+		gbc_btnRoll.insets = new Insets(0, 0, 0, 5);
+		gbc_btnRoll.gridx = 6;
+		gbc_btnRoll.gridy = 10;
+		add(btnRoll, gbc_btnRoll);
 
 	}
 
